df74b5ce8666e04a79e72ad72bd5c5ceb022acd4
yanguadotdev <yanguasamir05@gmail.com>
2025-06-10 21:05:08 -0500

    feat: displayed projects

--- src/data/site-data.ts
+++ src/data/site-data.ts
@@ -31,3 +31,127 @@ export const heroCopy = {
         }
     ]
 }
+
+
+export const projectsList = [
+    {
+        id: 'css-clamp-visualizer',
+        cover: '/projects/project_01.jpg',
+        title: 'CSS Clamp Visualizer',
+        repository: 'https://github.com/yanguadotdev/css-clamp-visualizer',
+        preview: 'https://css-clamp-visualizer.netlify.app/',
+        category: 'tool'
+    },
+    {
+        id: 'transform-origin-visualizer',
+        cover: '/projects/project_02.jpg',
+        title: 'Tranform Origin Visualizer',
+        repository: 'https://github.com/yanguadotdev/transform-origin-visualizer',
+        preview: 'https://transform-origin-visualizer.netlify.app/',
+        category: 'tool'
+    },
+    {
+        id: 'world-pets',
+        cover: '/projects/project_03.jpg',
+        title: 'World Pets',
+        repository: 'https://github.com/yanguadotdev/worldpets',
+        preview: 'https://worldpets.netlify.app/',
+        category: 'webApp'
+    },
+    {
+        id: 'spotify-clone',
+        cover: '/projects/project_04.jpg',
+        title: 'Spotify Clone',
+        repository: 'https://github.com/yanguadotdev/spotify-clone',
+        preview: 'https://spotify-clone-pi-pink.vercel.app/',
+        category: 'clone'
+    },
+    {
+        id: 'brainwave',
+        cover: '/projects/project_05.jpg',
+        title: 'Brainwave',
+        repository: 'https://github.com/yanguadotdev/brainwave',
+        preview: 'https://brainwave-beryl-ten.vercel.app/',
+        category: 'landing'
+    },
+    {
+        id: 'tic-tac-toe',
+        cover: '/projects/project_06.jpg',
+        title: 'Tic-Tac-Toe',
+        repository: 'https://github.com/yanguadotdev/tic-tac-toe-react',
+        preview: 'https://tic-tac-toe-full-react.netlify.app/',
+        category: 'webApp'
+    },
+    {
+        id: 'questions-answers',
+        cover: '/projects/project_15.jpg',
+        title: 'Preguntas sobre JS',
+        repository: 'https://github.com/yanguadotdev/javascript-this-exercises',
+        preview: 'https://yanguadotdev.github.io/javascript-this-exercises/',
+        category: 'landing'
+    },
+    {
+        id: 'pulse',
+        cover: '/projects/project_08.jpg',
+        title: 'Pulse',
+        repository: 'https://github.com/yanguadotdev/pulse',
+        preview: 'https://pulse-brand.netlify.app/',
+        category: 'landing'
+    },
+    {
+        id: 'layers',
+        cover: '/projects/project_09.jpg',
+        title: 'Layers',
+        repository: 'https://github.com/yanguadotdev/saas-landing-page',
+        preview: 'https://saas-landing-page-ten-nu.vercel.app/',
+        category: 'landing'
+    },
+    {
+        id: 'ecoscrap',
+        cover: '/projects/project_10.jpg',
+        title: 'Pulse',
+        repository: 'https://github.com/yanguadotdev/eco-scrap',
+        preview: 'https://eco-scrap-umber.vercel.app/',
+        category: 'landing'
+    },
+    {
+        id: 'halloween-hackaton',
+        cover: '/projects/project_11.jpg',
+        title: 'Hackaton Cloudinary',
+        repository: 'https://github.com/yanguadotdev/landing-cloudinary-hackathon',
+        preview: 'https://spooke-ia-cloudinary.netlify.app/',
+        category: 'landing'
+    },
+    {
+        id: 'movies-app',
+        cover: '/projects/project_12.jpg',
+        title: 'Movies DB',
+        repository: 'https://github.com/yanguadotdev/movies-app',
+        preview: 'https://movies-app-eight-pearl-90.vercel.app/',
+        category: 'webApp'
+    },
+    {
+        id: 'hirace',
+        cover: '/projects/project_13.jpg',
+        title: 'Hirace',
+        repository: 'https://github.com/yanguadotdev/hirace',
+        preview: 'https://hirace.vercel.app/',
+        category: 'landing'
+    },
+    {
+        id: 'barbersteel',
+        cover: '/projects/project_14.jpg',
+        title: 'BarberSteel',
+        repository: 'https://github.com/yanguadotdev/barbersteel',
+        preview: 'https://barbersteel.vercel.app/',
+        category: 'landing'
+    },
+    {
+        id: 'fit-life',
+        cover: '/projects/project_07.jpg',
+        title: 'Fit and Life',
+        repository: 'https://github.com/yanguadotdev/fitandlife',
+        preview: 'https://fitandlife.vercel.app/',
+        category: 'landing'
+    }
+]
