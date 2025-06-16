eb9518771bbdeb941efd15506769d8b66c66caea
yanguadotdev <yanguasamir05@gmail.com>
2025-06-16 11:47:26 -0500

    feat: update CV with new creative and professional version

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -12,6 +12,7 @@ import {
   SiCss3,
   SiReactrouter
 } from "react-icons/si";
+import { IoCloudDownloadOutline } from "react-icons/io5";
 
 
 const iconNameMap = new Map([
@@ -26,7 +27,8 @@ const iconNameMap = new Map([
   [SiHtml5, 'HTML5'],
   [SiVite, 'Vite'],
   [SiCss3, 'CSS3'],
-  [SiReactrouter, 'React Router']
+  [SiReactrouter, 'React Router'],
+  [IoCloudDownloadOutline, 'Download']
 ])
 
-export { SiReact, SiTailwindcss, SiNextdotjs, SiGit, SiAstro, SiJavascript, SiFigma, SiTypescript, SiHtml5, SiVite, SiCss3, SiReactrouter, iconNameMap };
+export { SiReact, SiTailwindcss, SiNextdotjs, SiGit, SiAstro, SiJavascript, SiFigma, SiTypescript, SiHtml5, SiVite, SiCss3, SiReactrouter, IoCloudDownloadOutline, iconNameMap };
